46bdd69c8fd5ec848ffafdd1e299f296efd18ab7
Lucas Augusto <lucas.augusto5061@gmail.com>
2015-12-02 21:36:55 -0200

    Iniciado projeto de angularJs

---
+++ trilha_front_end/angular/public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function ($scope) {
+	$scope.foto = {
+		titulo: 'Leão',
+		url: 'https://i.ytimg.com/vi/92r4CSt8txw/maxresdefault.jpg'
+	};
+});
