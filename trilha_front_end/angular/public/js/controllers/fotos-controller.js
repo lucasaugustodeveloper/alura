3939ba4503eb16c4589de4010de4d97b7d43c82f
Lucas Augusto <lucas.augusto5061@gmail.com>
2015-12-04 19:54:54 -0200

    Terminado curso de angularJS

--- trilha_front_end/angular/public/js/controllers/fotos-controller.js
+++ trilha_front_end/angular/public/js/controllers/fotos-controller.js
@@ -1,6 +1,23 @@
-angular.module('alurapic').controller('FotosController', function ($scope) {
-	$scope.foto = {
-		titulo: 'Leão',
-		url: 'https://i.ytimg.com/vi/92r4CSt8txw/maxresdefault.jpg'
+angular.module('alurapic').controller('FotosController', function ($scope, recursoFoto) {
+	$scope.fotos = [];
+	$scope.filtro = '';
+	$scope.mensagem = '';
+
+	recursoFoto.query(function(fotos){
+		$scope.fotos = fotos;
+	},function(erro){
+		console.log(erro);
+	});
+
+	$scope.remover = function(foto){
+
+		recursoFoto.delete({fotoId : foto._id}, function(){
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto, 1);
+			$scope.mensagem = 'Foto ' + foto.titulo + ' foi apagada com sucesso';
+		},function(erro){
+			console.log(erro);
+			$scope.mensagem = 'Não foi possivel apagar a foto' + foto.titulo;
+		});
 	};
 });
