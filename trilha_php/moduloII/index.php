44762b6019d8508712203d241db325d7fba9d1f0
Lucas Augusto <lucas.augusto5061@gmail.com>
2016-01-26 12:34:24 -0200

    Inicado modificações para fazer login de adm

--- trilha_php/moduloII/index.php
+++ trilha_php/moduloII/index.php
@@ -4,35 +4,43 @@
 	<h1>Seja Bem Vindo!!!</h1>
 	<h2>Curso de PHP e MySQL I Alura</h2>
 </div>
-<?php if(isset($_GET["login"]) && $_GET["login"] == "true"){ ?>
-		<p class="alert alert-success">Logado com sucesso.</p>
-<?php } ?>
-
-<?php if(isset($_GET["login"]) && $_GET["login"] == "false"){ ?>
-		<p class="alert alert-success">Logado com sucesso.</p>
-<?php } ?>
+<?php 
+	if(isset($_GET["login"]) && $_GET["login"] == "true"){ 
 ?>
-<div class="container">
-	<div class="row login">
-		<h2 class="text-center">Login</h2>
-		<form action="login.php" method="post">
-			<table class="table table-bordered">
-				<tr>
-					<td><label for="email">Email:</label></td>
-					<td><input type="email" class="form-control" id="email"></td>
-				</tr>
-				<tr>
-					<td><label for="senha">Senha:</label></td>
-					<td><input type="password" class="form-control" id="senha"></td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<button type="submit" class="btn btn-primary btn-block">Login</button>
-					</td>
-				</tr>
-			</table>
-		</form>
+		<p class="alert alert-success">Logado com sucesso.</p>
+<?php
+	}
+?>
+
+<?php 
+	if(isset($_GET["login"]) && $_GET["login"] == "false"){
+?>
+		<p class="alert alert-success">Logado com sucesso.</p>
+<?php 
+	}
+?>
+?>
+	<div class="container">
+		<div class="row login">
+			<h2 class="text-center">Login</h2>
+			<form action="login.php" method="post">
+				<table class="table table-striped table-bordered">
+					<tr>
+						<td><label for="email">Email:</label></td>
+						<td><input type="email" class="form-control" name="email" id="email"></td>
+					</tr>
+					<tr>
+						<td><label for="senha">Senha:</label></td>
+						<td><input type="password" class="form-control" senha="senha" id="senha"></td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<button type="submit" class="btn btn-primary btn-block">Login</button>
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
 	</div>
-</div>
 
 <?php include("includes/footer.php"); ?>
--- trilha_php/moduloII/login.php
+++ trilha_php/moduloII/login.php
@@ -2,11 +2,12 @@
 	include("conectar.php");
 	include("banco-usario.php");
 
-	$usuario = buscaUsuario($conexao, $POST["email"], $_POST["senha"]);
-	if($usuario == null){
-		header("Location: index.php?login=0");
-	}else{
-		header("Location: index.php?login=1");
-	}
+	$usuario = buscaUsuario($conexao, $_POST["email"], $_POST["senha"]);
+	var_dump($usuario);
+// 	if($usuario == null){
+// 		header("Location: index.php?login=0");
+// 	}else{
+// 		header("Location: index.php?login=1");
+// 	}
 
-die();
+// die();
