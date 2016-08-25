6e445117448368512e87f73b1c2f86485ac11874
Lucas Augusto <lucas.augusto5061@gmail.com>
2016-08-25 17:48:16 -0300

    Finish modulo javascript advanced I

--- trilha_front_end/javascript_avancado/client/js/app/controllers/NegociacaoController.js
+++ trilha_front_end/javascript_avancado/client/js/app/controllers/NegociacaoController.js
@@ -25,6 +25,15 @@ class NegociacaoController {
 		this._limpaFormulario();
 	}
 
+	apaga() {
+
+		this._listaNegociacoes.esvazia();
+		this._negociacoesView.update(this._listaNegociacoes);
+
+		this._mensagem.texto = 'Lista de Negociações apagada com sucesso';
+		this._mensagemView.update(this._mensagem);
+	}
+
 	_criaNegociacao() {
 		return new Negociacao(
 					DateHelper.textoParaData(this._inputData.value),
